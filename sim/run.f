cdb3b1b4a2b16ec2fcfd8193cecbba15fc35ca0a
nareshp235 <naresh.p235@gmail.com>
2026-06-12 16:44:38 +0530

    Create run.f

---
+++ sim/run.f
@@ -0,0 +1,14 @@
++incdir+tb/common
++incdir+rtl/interfaces
++incdir+rtl/gpio
++incdir+tb/bfm
+
+tb/common/params_pkg.sv
+
+rtl/interfaces/apb_if.sv
+
+rtl/gpio/gpio_apb.sv
+
+tb/bfm/apb_master_bfm.sv
+
+tb/tb_top.sv
